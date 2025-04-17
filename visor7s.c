eaef3d516a451e8866501c33c61eeaa78a8820ec
sleivaolsen <sebastian.leiva@biomedica.udec.cl>
2025-04-17 02:09:36 -0400

    Update visor7s.c
    copié código de ejemplo 5

--- visor7s.c
+++ visor7s.c
@@ -1,49 +1,117 @@
+//Distintas opciones para trabajar visores de 7 segmentos
 //requiere definir pines de transistores
 //y pines de visores de 7 segmentos
 
 //Pendiente averiguar 8 pines con direcciones de memoria contiguas
-A=0;B=0;C=0;D=0;E=0;F=0;G=0;dp=0;
+//Para poder usar elementos de un vector "anodo[num]" o "catodo[num]"
+//uint8_t catodo[]={0b1111110,0b0110000,0b1101101,0b1111001,0b0110011,5,6,7,8,9} 
+//A=0;B=0;C=0;D=0;E=0;F=0;G=0;dp=0;
 
-void visor(int num);
+void visor(uint8_t num);
 void barrido(int num);
 
-//escribe un dígito en un display de 7 segmentos. Invertir valores para cátodo comun...
-void visor(num){
-	switch(num){
-  	case 0:
-    	A=1;B=1;C=1;D=1;E=1;F=1;G=0;	//     A
-  		break; 												//  F     B
-    case 1: 												//     G
-      A=0;B=1;C=1;D=0;E=0;F=0;G=0;	//  E     C
-			break; 												//     D   dp
-    case 2: 												
-      A=1;B=1;C=0;D=1;E=1;F=0;G=1;
-      break; 
-    case 3:
-      A=1;B=1;C=1;D=1;E=0;F=0;G=1;
-      break;
-    case 4:
-      A=0;B=1;C=1;D=0;E=0;F=1;G=1;
-      break;
-    case 5:
-      A=1;B=0;C=1;D=1;E=0;F=1;G=1;
-      break;
-    case 6:
-      A=1;B=0;C=1;D=1;E=1;F=1;G=1;
-      break;
-    case 7:
-      A=1;B=1;C=1;D=0;E=0;F=0;G=0;
-      break;
-		case 8:
-      A=1;B=1;C=1;D=1;E=1;F=1;G=1;
-      break;
-		case 9:
-      A=1;B=1;C=1;D=1;E=0;F=1;G=1;
-      break;
-  }
+//escribe un dígito en un display de 7 segmentos de cátodo común. Invertir valores para ánodo comun...
+void visor(uint8_t num)
+{
+	switch(num)
+	{
+	case 0:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 1);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 1);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 1);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 0);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 0);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 0);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 0);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 0);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 0);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 0);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 1);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 1);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 0);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 1);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 0);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 0);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 0);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 0);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 0);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 1);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 0);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 1);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 0);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 1);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 0);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 1);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 1);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 1);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 0);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 0);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 0);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 0);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 1);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 1);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 1);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(DISP_A_GPIO_Port, DISP_A_Pin, 1);
+		HAL_GPIO_WritePin(DISP_B_GPIO_Port, DISP_B_Pin, 1);
+		HAL_GPIO_WritePin(DISP_C_GPIO_Port, DISP_C_Pin, 1);
+		HAL_GPIO_WritePin(DISP_D_GPIO_Port, DISP_D_Pin, 0);
+		HAL_GPIO_WritePin(DISP_E_GPIO_Port, DISP_E_Pin, 0);
+		HAL_GPIO_WritePin(DISP_F_GPIO_Port, DISP_F_Pin, 1);
+		HAL_GPIO_WritePin(DISP_G_GPIO_Port, DISP_G_Pin, 1);
+		break;
+	}
 }
 
 //para asegurar una tasa de refresco constante para todos los displays, llamaremos a barrido() usando interrupciones de Timer
+//Interrupción por Timer cada 10ms, en la que se llama a barrido(angulo_promedio[modo]), por ejemplo.
+//modo=1 muestra x, modo=2 muestra y, modo=3 muestra z... El cambio de modo lo vemos en el while del main.
+
 void barrido(num){
 	transistor++;
 	switch(transistor){
@@ -59,6 +127,3 @@ void barrido(num){
 	  transistor=0;
 	break;
 }	
-
-//Interrupción por Timer cada 10ms, en la que se llama a barrido(angulo_promedio[modo])
-//modo=1 muestra x, modo=2 muestra y, modo=3 muestra z... 
